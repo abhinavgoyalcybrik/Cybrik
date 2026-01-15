ed81e42a0520537e1cec9af8890e6d5e2561b002
abhinavgoyalcybrik <abhinav.g@cybriksolutions.com>
2026-01-15 11:51:18 +0530

    Fix: Add missing DjangoFilterBackend import

--- Backend/CybricHQ/crm_app/views.py
+++ Backend/CybricHQ/crm_app/views.py
@@ -28,6 +28,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django_filters.rest_framework import DjangoFilterBackend
 from .authentication import JWTAuthFromCookie
 
 from .models import (
